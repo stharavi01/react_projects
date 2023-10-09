f1007afddbfd71f20abfbab93b5ce1b0637451f9
Rabin Shrestha <sanayashe@gmail.com>
2023-10-09 13:21:23 +0545

    final commit

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	return (
+		<div>
+			{" "}
+			<h2>lorem ipsum project setup</h2>
+			<h3>lorem ipsum project setup</h3>
+		</div>
+	);
 }
 
 export default App;
