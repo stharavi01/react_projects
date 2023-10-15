ef3dfe6b8dd3a4b38c460fb8fd1178dca5afc092
Rabin Shrestha <sanayashe@gmail.com>
2023-10-15 12:27:02 +0545

    final commit

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
-
+// Menu setup
 function App() {
 	const [menuItems, setMenuItems] = useState(items);
 	const [categories, setCategories] = useState(allCategories);
