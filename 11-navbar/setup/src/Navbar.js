97c60360e583fe7b8285da924c6a6037d093504b
Rabin Shrestha <sanayashe@gmail.com>
2023-10-15 18:47:41 +0545

    Add toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,6 +4,7 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+	const [showLinks, setShowLinks] = useState(false);
 	return (
 		<nav>
 			<div className="nav-center">
@@ -13,38 +14,31 @@ const Navbar = () => {
 						<FaBars />
 					</button>
 				</div>
-				<div className="links-container show-container">
+				<div
+					className={`${
+						showLinks ? "links-container show-container" : "links-container"
+					}`}
+				>
 					<ul className="links">
-						<li>
-							<a href="#">Home</a>
-						</li>
-						<li>
-							<a href="#">About</a>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-						<li>
-							<a href="#">Products</a>
-						</li>
+						{links.map((link) => {
+							const { id, url, text } = link;
+							return (
+								<li key={id}>
+									<a href={url}>{text}</a>
+								</li>
+							);
+						})}
 					</ul>
 				</div>
 				<ul className="social-icons">
-					<li>
-						<a href="https://www.twitter.com">
-							<FaTwitter />
-						</a>
-					</li>
-					<li>
-						<a href="https://www.twitter.com">
-							<FaTwitter />
-						</a>
-					</li>
-					<li>
-						<a href="https://www.twitter.com">
-							<FaTwitter />
-						</a>
-					</li>
+					{social.map((socialIcon) => {
+						const { id, url, icon } = socialIcon;
+						return (
+							<li key={id}>
+								<a href={url}>{icon}</a>
+							</li>
+						);
+					})}
 				</ul>
 			</div>
 		</nav>
