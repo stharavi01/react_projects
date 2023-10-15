24d4cdce72329232d8482dc4f80961cbbed72f4a
Rabin Shrestha <sanayashe@gmail.com>
2023-10-15 12:14:57 +0545

    final commit

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -6,11 +6,20 @@ function App() {
 	const [people, setPeople] = useState(data);
 	const [index, setIndex] = useState(0);
 
-	// useEffect(()=>{
-	//   return(
+	useEffect(() => {
+		const lastIndex = people.length - 1;
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		if (index > lastIndex) setIndex(0);
+	}, [index, people]);
 
-	//   );
-	// },[])
+	useEffect(() => {
+		let slider = setInterval(() => {
+			setIndex(index + 1);
+		}, 3000);
+		return () => clearInterval(slider);
+	}, [index]);
 	return (
 		<section className="section">
 			<div className="title">
@@ -43,10 +52,10 @@ function App() {
 						</article>
 					);
 				})}
-				<button className="prev">
+				<button className="prev" onClick={() => setIndex(index - 1)}>
 					<FiChevronLeft />
 				</button>
-				<button className="next">
+				<button className="next" onClick={() => setIndex(index + 1)}>
 					<FiChevronRight />
 				</button>
 			</div>
