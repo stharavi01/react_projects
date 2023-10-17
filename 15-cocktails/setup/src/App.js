2332ea44fa737b2a31d1744d9cbee85551b57563
Rabin Shrestha <sanayashe@gmail.com>
2023-10-17 22:58:44 +0545

    lorem

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -18,6 +18,12 @@ function App() {
 				placeat nobis mollitia nam, quos repudiandae quo magnam maxime
 				architecto reiciendis neque unde.
 			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+				doloremque id consequatur minus enim similique obcaecati accusantium
+				placeat nobis mollitia nam, quos repudiandae quo magnam maxime
+				architecto reiciendis neque unde.
+			</p>
 		</div>
 	);
 }
