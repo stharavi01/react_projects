498d3a8dfaef69853086020ebc2437a1b1335606
Rabin Shrestha <sanayashe@gmail.com>
2023-10-18 15:37:03 +0545

    error and about page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">About Us</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti optio
+				quidem debitis libero dolorem error dolorum nam minus hic doloribus
+				earum eum dolore corporis ratione labore, nisi at temporibus eligendi
+				tempora, amet sequi non. Quis consectetur iusto velit beatae doloribus.
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
