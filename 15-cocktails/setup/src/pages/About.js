b235b07398692e1e18e03f0d72429c9ce97e888c
Rabin Shrestha <sanayashe@gmail.com>
2023-10-18 16:42:56 +0545

    about js

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -5,10 +5,10 @@ const About = () => {
 		<section className="section about-section">
 			<h1 className="section-title">About Us</h1>
 			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti optio
-				quidem debitis libero dolorem error dolorum nam minus hic doloribus
-				earum eum dolore corporis ratione labore, nisi at temporibus eligendi
-				tempora, amet sequi non. Quis consectetur iusto velit beatae doloribus.
+				Lorem, ipsum dolor sit amet quidem debitis libero dolorem error dolorum
+				nam minus hic doloribus earum eum dolore corporis ratione labore, nisi
+				at temporibus eligendi tempora, amet sequi non. Quis consectetur iusto
+				velit beatae doloribus.
 			</p>
 		</section>
 	);
