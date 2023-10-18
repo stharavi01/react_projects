3a555887082a8965d19bf387c7016dc5665f1830
Rabin Shrestha <sanayashe@gmail.com>
2023-10-18 15:14:52 +0545

    setup react router

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import pages
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,11 +7,26 @@ import SingleCocktail from "./pages/SingleCocktail";
 import Error from "./pages/Error";
 // import components
 import Navbar from "./components/Navbar";
+import Cocktail from "./components/Cocktail";
 function App() {
 	return (
-		<div>
-			<h2>app component</h2>
-		</div>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/cocktail/:id">
+					<Cocktail />
+				</Route>
+				<Route path="*">
+					<Error />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
